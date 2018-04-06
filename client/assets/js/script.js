921d9591c71ecbb0bf206a8ab04c2089c771a235
sagehornung <sagehornung@gmail.com>
2018-04-05 17:07:27 -0700

    QAddefd some nonsense

--- client/assets/js/script.js
+++ client/assets/js/script.js
@@ -1,4 +1,12 @@
 var app = angular.module("app", []);
+
 app.controller("HelloController", function($scope) {
     $scope.message = "Hello, AngularJS";
+    $scope.yo = 2 + 2;
+    $scope.test_three = 'Fun stuff';
+});
+
+app.controller("HelloController2", function($scope) {
+    $scope.message = "Hello, AngularJS";
+    $scope.test = 0;
 });
